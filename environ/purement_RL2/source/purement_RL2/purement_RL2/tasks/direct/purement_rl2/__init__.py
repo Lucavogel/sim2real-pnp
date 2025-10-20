7cd6cad530b4f830ccddf6cdba005467f9cdd059
Ajin3486 <luca.vogelgesang@etu.umontpellier.fr>
2025-10-20 14:38:52 +0200

    Add purement_RL2 environment with IsaacLab template

---
+++ environ/purement_RL2/source/purement_RL2/purement_RL2/tasks/direct/purement_rl2/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Purement-Rl2-Direct-v0",
+    entry_point=f"{__name__}.purement_rl2_env:PurementRl2Env",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.purement_rl2_env_cfg:PurementRl2EnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
